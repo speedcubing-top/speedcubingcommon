262651136afe53bb2c82f12e5df93de61fdf15a5
speedcubing <speedcubing@speedcubing.top>
2026-08-12 00:39:07 +0800

    onlinecount empty check

--- src/main/java/top/speedcubing/common/CubingTick.java
+++ src/main/java/top/speedcubing/common/CubingTick.java
@@ -1,11 +1,13 @@
 package top.speedcubing.common;
 
-import java.util.Timer;
-import java.util.TimerTask;
 import top.speedcubing.common.database.Database;
 import top.speedcubing.common.database.DatabaseData;
 import top.speedcubing.common.events.CubingTickEvent;
 import top.speedcubing.lib.utils.SQL.SQLConnection;
+import top.speedcubing.lib.utils.SQL.SQLResult;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class CubingTick {
     public static Timer tickTimer;
@@ -21,7 +23,11 @@ public class CubingTick {
                      SQLConnection system = Database.getSystem()) {
                     DatabaseData.champs.clear();
                     cubing.select("id").from("champ").executeResult().forEach(r -> r.forEach(f -> DatabaseData.champs.add(f.getInt())));
-                    DatabaseData.onlineCount = system.select("SUM(onlinecount)").from("proxies").executeResult().getInt();
+
+                    SQLResult result = system.select("SUM(onlinecount)").from("proxies").executeResult();
+                    if (!result.isEmpty()) {
+                        DatabaseData.onlineCount = result.getInt();
+                    }
                     tick++;
                     CubingTickEvent event = new CubingTickEvent(tick);
                     event.call();
